ae5cac591e0b010109e362bd658c0a98944a6d4d
eirikjak <eirikjak@gmail.com>
2014-01-07 23:45:00 +0100

    Moves LevelState class defenition to a script file

--- Assets/LevelController.js
+++ Assets/LevelController.js
@@ -1,21 +1,5 @@
 ﻿#pragma strict
 
-public static class LevelState{
-	//The number of circlings that has reached the goal
-	public static var CirclingsInGoal:int;
-	//The number of circlings that are active or waiting for launch
-	public static var LiveCirclings:int;
-	//The number of coins collected
-	public static var CollectedCoins:int;
-	
-	public static function Reset(){
-		CirclingsInGoal = 0;
-		LiveCirclings = 0;
-		CollectedCoins = 0;
-	}
-
-}
-
 public var MinimumGoal:int = 0;
 public var LevelContainer:GameObject;
 public var Menu:GameObject;
---
+++ Assets/LevelState.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+public static class LevelState{
+	//The number of circlings that has reached the goal
+	public static var CirclingsInGoal:int;
+	//The number of circlings that are active or waiting for launch
+	public static var LiveCirclings:int;
+	//The number of coins collected
+	public static var CollectedCoins:int;
+	
+	public static function Reset(){
+		CirclingsInGoal = 0;
+		LiveCirclings = 0;
+		CollectedCoins = 0;
+	}
+
+}
