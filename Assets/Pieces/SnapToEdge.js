b16cb1336363a15a960561da7317d788eef4f16c
eirikjak <eirikjak@gmail.com>
2014-01-25 14:34:25 +0100

    improved snap to edge

--- Assets/Pieces/SnapToEdge.js
+++ Assets/Pieces/SnapToEdge.js
@@ -35,14 +35,18 @@ class SnapToEdge extends MonoBehaviour implements DragInputListener{
 			for(var j in [-1,1]){
 			var pos = this.transform.position;
 			var corner = pos - Vector2.Scale(bounds.extents,Vector2(i,j));
-			var horizontalRay = Physics2D.Raycast(corner,-Vector2.right*i,Distance, ~((1 << this.gameObject.layer) | 1 << LayerMask.NameToLayer("GUI")));
+			var horizontalRayOut = Physics2D.Raycast(corner,-Vector2.right*i,Distance, ~((1 << this.gameObject.layer) | 1 << LayerMask.NameToLayer("GUI")));
+			var horizontalRayIn = Physics2D.Raycast(corner,Vector2.right*i,Distance, ~((1 << this.gameObject.layer) | 1 << LayerMask.NameToLayer("GUI")));
 			var verticalRay = Physics2D.Raycast(corner,-Vector2.up*j,Distance, ~((1 << this.gameObject.layer) | 1 << LayerMask.NameToLayer("GUI")));
 			//Debug
-			Debug.DrawRay(this.transform.position - Vector3.Scale(bounds.extents,Vector3(i,j,0)),-Vector2.right*i*Distance,!horizontalRay.collider ? Color.black: Color.red);
+			Debug.DrawRay(this.transform.position - Vector3.Scale(bounds.extents,Vector3(i,j,0)),-Vector2.right*i*Distance,!horizontalRayOut.collider ? Color.black: Color.red);
+			Debug.DrawRay(this.transform.position - Vector3.Scale(bounds.extents,Vector3(i,j,0)),Vector2.right*i*Distance,!horizontalRayIn.collider ? Color.black: Color.red);
 			Debug.DrawRay(this.transform.position - Vector3.Scale(bounds.extents,Vector3(i,j,0)),-Vector2.up*j*Distance, !verticalRay.collider ? Color.black: Color.red);
 			
-			if(horizontalRay.collider){
-				this.transform.position.x -= corner.x - horizontalRay.point.x;
+			if(horizontalRayOut.collider){
+				this.transform.position.x -= corner.x - horizontalRayOut.point.x;
+			}else if(horizontalRayIn.collider){
+				this.transform.position.x -= corner.x - horizontalRayIn.point.x;
 			}
 			if(verticalRay.collider){
 				
